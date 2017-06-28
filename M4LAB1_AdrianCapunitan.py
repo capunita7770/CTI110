e0e3eced26941d084a4a9da1272d101a48460ec9
capunita7770 <capunita7770@student.faytechcc.edu>
2017-06-28 10:11:29 -0400

    Add files via upload

---
+++ M4LAB1_AdrianCapunitan.py
@@ -0,0 +1,20 @@
+# Turtle
+# 6/20
+# CTI-110 Turtle Graphics Program
+# Adrian Capunitan
+#
+
+import turtle
+win = turtle.Screen()
+adrian = turtle.Turtle()
+
+for i in range (4):
+    adrian.forward (50)
+    adrian.left (90)
+
+for i in range (3):
+    adrian.forward (120)
+    adrian.left (120)
+    
+win.mainloop()
+
---
+++ M4LAB2_AdrianCapunitan.py
@@ -0,0 +1,31 @@
+# Initial
+# 6/20
+# CTI-110 Turtle Graphics Program
+# Adrian Capunitan
+#
+
+import turtle
+win = turtle.Screen()
+t = turtle.Turtle()
+
+t.pensize (5)
+t.pencolor ("blue")
+t.shape ("turtle")
+
+t.forward (70)
+t.right (70)
+t.forward (70)
+t.backward (170)
+t.right (40)
+t.forward (170)
+t.left (110)
+t.penup ()
+t.forward (150)
+t.pencolor ("red")
+t.pendown ()
+t.forward (80)
+t.backward (80)
+t.left (90)
+t.forward (160)
+t.right (90)
+t.forward (80)
