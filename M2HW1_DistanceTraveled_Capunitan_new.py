00762ce5075a24c268deeb2583bf6ed7f274b1b8
capunita7770 <capunita7770@student.faytechcc.edu>
2017-07-19 13:03:53 -0400

    Add files via upload

---
+++ M2HW1_DistanceTraveled_Capunitan_new.py
@@ -0,0 +1,20 @@
+# Distance Traveled
+# 6/7
+# CTI-110 M2HW1 - Distance Traveled 
+# Adrian Capunitan
+# 
+
+# Distance Traveled.
+
+# Distance = Speed * Time
+
+# A car travellingat 70 miles per hour. 
+# The distance the car will travel in 6 hours
+# The distance the car will travel in 10 hours
+# The distance the car will travel in 15 hours
+
+speed = 70
+print ( "In 6 hour, the car will travel",speed * 6,"miles"  )
+print ( "In 10 hour, the car will travel",speed * 10,"miles"  )
+print ( "In 15 hour, the car will travel",speed * 15,"miles"  )
+
